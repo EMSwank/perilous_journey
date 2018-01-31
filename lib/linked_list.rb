b45488b2a47b342e005a103b5ab26bf711ae4b84
Eliot Swank <eliotswank@gmail.com>
2018-01-31 09:33:28 -0700

    find method completed

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -66,6 +66,16 @@ class LinkedList
         current_node.next_node = new_node
     end
 
-    def find
+    def find(index, elements)
+        new_family_list = LinkedList.new
+        current_node = @head 
+        index.times { current_node = current_node.next_node}
+        new_head = current_node
+        new_family_list.append(new_head.surname)
+        (elements - 1).times do
+            current_node = current_node.next_node
+            new_family_list.append(current_node.surname)
+        end
+        new_family_list.to_string
     end
 end
