289ca7871c03edcb6c5cd58ba63f3202d185fe56
Eliot Swank <eliotswank@gmail.com>
2018-01-31 18:41:26 -0700

    Iteration 5 complete

--- test/wagon_train_test.rb
+++ test/wagon_train_test.rb
@@ -2,12 +2,33 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/node'
 require './lib/linked_list'
+require './lib/wagon_train'
 
 class WagonTrainTest < Minitest::Test
     def test_it_exists
-        wagon = WagonTrain.new
+        wt = WagonTrain.new
 
-        assert_instance_of wagon, WagonTrain
+        assert_instance_of WagonTrain, wt
     end
     
+    def test_it_returns_list_head_is_nil
+        wt = WagonTrain.new
+    
+        assert_nil wt.head
+    end
+
+    def test_it_can_append
+        wt = WagonTrain.new
+
+        assert_equal "Burke", wt.append("Burke").surname
+    end
+
+    def test_it_can_count_wagons
+        wt = WagonTrain.new
+        
+        wt.append("Burke")
+        wt.append("West")
+        
+        assert_equal 2, wt.count
+    end
 end
